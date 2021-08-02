98c4849179bf9e1009702c87d64a8a45fe5a1c07
Justin <jusglinton@gmail.com>
2021-08-02 17:31:44 -0400

    react

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
+  //use effect run once
+  useEffect(() =>{
+    getLocalTodos();
+  }, []);
   //use effect
   useEffect( () =>{
     filterHandler();
+    saveLocalTodos();
   },[todos, status] );
 
   //functions
@@ -26,8 +31,20 @@ function App() {
       default:
         setFilteredTodos(todos)
         break;
+    } 
+  };
+
+  const saveLocalTodos = () =>{
+   localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const getLocalTodos = () =>{
+    if (localStorage.getItem("todos")=== null){
+      localStorage.setItem("todos", JSON.stringify([]));
+    }else{
+     let todoLocal = JSON.parse(localStorage.getItem("todos"));
+     setTodos(todoLocal);
     }
-    
   }
   return (
     <div className="App">
